b015cab93f94d0b0dbdd11342f79f5df34b2797b
hananeaouragh-ops <hananodesu976@gmail.com>
2026-05-01 00:00:27 +0100

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,13 +1,15 @@
- #include <Adafruit_MPU6050.h>
+#include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 Adafruit_MPU6050 mpu;
 
 void setup() {
+  // Initialize serial communication at 115200 baud
   Serial.begin(115200);
   while (!Serial) delay(10); 
 
+  // Initialize MPU6050 sensor
   if (!mpu.begin()) {
     while (1) yield();
   }
@@ -15,14 +17,17 @@ void setup() {
 
 void loop() {
   sensors_event_t a, g, temp;
+  
+  // Read sensor data
   mpu.getEvent(&a, &g, &temp);
 
-  // Format l-arkam bach i-kono sahline f l-copy
+  // Format output as CSV for data collection (Accel X, Y, Z)
   Serial.print(a.acceleration.x);
   Serial.print(",");
   Serial.print(a.acceleration.y);
   Serial.print(",");
   Serial.println(a.acceleration.z);
 
+  // Maintain 50Hz sampling rate
   delay(20); 
 }
